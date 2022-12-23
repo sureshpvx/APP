18d86923022283f5638b1fb6825bbc9f5719f0ed
sureshpvx <sureshsharma04885@gmail.com>
2022-12-23 14:29:34 +0530

    complete project

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,7 +33,7 @@ class ArticlesController < ApplicationController
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
-      render 'edit'
+      render 'edit', status: :unprocessable_entity
     end
   end
 
