0904348310c0e6f3e9dfb21c62ba2f2164f57215
sureshpvx <sureshsharma04885@gmail.com>
2022-12-28 11:56:13 +0530

    connection between user and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,11 +19,12 @@ class ArticlesController < ApplicationController
 
   def create
   	@article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
     else
-      render 'new'
+      render 'new' ,status: 422
     end
   end
 
