f8d1a5f9d8bd07d2bed9c056d69f099c1418f2db
sureshpvx <sureshsharma04885@gmail.com>
2023-01-02 16:54:54 +0530

    complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_30_121535) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_101725) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -19,11 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_30_121535) do
     t.integer "user_id"
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  
 
   create_table "users", force: :cascade do |t|
     t.string "username"
